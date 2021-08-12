36f2837197f7e8a1eddce7524a4596353f1f23f5
Anna <am-rabota@mail.ru>
2021-08-12 14:55:56 -0700

    random health and attak values

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,14 +22,15 @@ var fight = function(enemyName) {
             if (confirmSkip) {
             window.alert(playerName + " has decided to skip this fight. Goodbye!");
             // subtract money from playerMoney for skipping
-            playerMoney = playerMoney - 10;
+            playerMoney = Math.max(0, playerMoney - 10);
             console.log("playerMoney", playerMoney)
             break;
             }
         }
         
-        // remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        // generate random damage value based on player's attack power
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+        enemyHealth = Math.max(0, enemyHealth - damage);
         console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
         );
         // check enemy's health
@@ -43,8 +44,8 @@ var fight = function(enemyName) {
             window.alert(enemyName + " still has " + enemyHealth + " health left.");
         }
     
-        // remove player's health by subtracting the amount set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+        playerHealth = Math.max(0, playerHealth - damage);
         console.log(enemyName + " attacked " + playerName + ". "
             + playerName + " now has " + playerHealth + " health remaining.");
     
@@ -73,7 +74,7 @@ var startGame = function() {
             // pick new enemy to fight based on the index of the enemyNames array
             var pickedEnemyName = enemyNames[i];
             // reset enemyHealth before starting new fights
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
             // use debugger to pause script from running and check what's going on at that moment in the code
             // debugger;
             // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
@@ -107,7 +108,7 @@ var endGame = function() {
     else {
         window.alert("You've lost your robot in battle.");
     }
-}
+};
 
 // ask player if they'd like to play again
 var playAgainConfirm = window.confirm("Would you like to play again?");
@@ -163,6 +164,11 @@ switch (shopOptionPrompt) {
             break;
     }
 };
-
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+  
+    return value;
+};
 // start first game when page loads
 startGame();
